52f0354efdf9b7e84203f8844620cd12316f6b8f
rajesh6161 <rajeshranjan2712000@gmail.com>
2020-03-14 23:36:18 +0530

    made more changes 95%complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DeleteData from "./components/DeleteData";
 const App = () => {
   return (
     <div>
+      <h2 className="center">Listing Data</h2>
       <GetData />
       <CreateData />
       <UpdateData />
--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Flip from "react-reveal/Flip";
 class GetData extends Component {
   state = {
     datas: []
@@ -19,44 +20,48 @@ class GetData extends Component {
     const list = datas.length ? (
       datas.map(post => {
         return (
-          <div className="cadets-div" key={post.id}>
-            <p>
-              id: <span>{post.id}</span>
-            </p>
-            <hr />
-            <p>
-              Name: <span>{post.name}</span>
-            </p>
-            <hr />
-            <p>
-              Venue: <span>{post.venue}</span>
-            </p>
-            <hr />
-            <p>
-              Date:{" "}
-              <span>{post.date === null ? "Date Unavailable" : post.date}</span>
-            </p>
-            <hr />
-            <p>
-              Time: <span>{post.time}</span>
-            </p>
-            <hr />
-            <p>
-              Details: <span>{post.details}</span>
-            </p>
-            <hr />
-            <p>
-              Email: <span>{post.email}</span>
-            </p>
-            <hr />
-            <p>
-              Year: <span>{post.year}</span>
-            </p>
-          </div>
+          <Flip>
+            <div className="cadets-div" key={post.id}>
+              <p>
+                id: <span>{post.id}</span>
+              </p>
+              <hr />
+              <p>
+                Name: <span>{post.name}</span>
+              </p>
+              <hr />
+              <p>
+                Venue: <span>{post.venue}</span>
+              </p>
+              <hr />
+              <p>
+                Date:{" "}
+                <span>
+                  {post.date === null ? "Date Unavailable" : post.date}
+                </span>
+              </p>
+              <hr />
+              <p>
+                Time: <span>{post.time}</span>
+              </p>
+              <hr />
+              <p>
+                Details: <span>{post.details}</span>
+              </p>
+              <hr />
+              <p>
+                Email: <span>{post.email}</span>
+              </p>
+              <hr />
+              <p>
+                Year: <span>{post.year}</span>
+              </p>
+            </div>
+          </Flip>
         );
       })
     ) : (
-      <button className="loadingBtn"></button>
+      <h3 className="center">Getting Data.....</h3>
     );
     return <div className="container displayData">{list}</div>;
   }
